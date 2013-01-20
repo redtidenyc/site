6df59fd58e2af35d57c937bf188b472fe4abcf40
Anna Armentrout <atrout@pipeline.com>
2013-01-19 23:08:57 -0500

    playlist?

--- rt_www/photogallery/urls.py
+++ rt_www/photogallery/urls.py
@@ -4,6 +4,6 @@ from rt_www.photogallery.models import Video
 
 urlpatterns = patterns('',
     (r'^$', 'django.views.generic.simple.direct_to_template', { 'template':'photogallery/index.html' }),
-    (r'^videos', 'rt_www.photogallery.views.videos'),    
-    (r'^playlist', 'rt_www.photogallery.views.playlist'),
+    (r'^videos', 'rt_www.photogallery.views.videos')    
+    #(r'^playlist', 'rt_www.photogallery.views.playlist'),
 )
